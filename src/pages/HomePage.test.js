5c4f67c3c494c726050a2ba1a2087d7cc6d12d4d
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-19 20:58:22 +0100

    Added test to homepage

--- src/pages/HomePage.test.js
+++ src/pages/HomePage.test.js
@@ -1,10 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../setupTests";
 import HomePage from "./HomePage";
 
 describe("Given a RobotDisplay component", () => {
   describe("When it receives a robot", () => {
     test("Then it should display a heading with the text robot", () => {
-      render(<HomePage />);
+      renderWithProviders(<HomePage />);
 
       const title = screen.getByRole("heading", { name: /robots list/i });
 
