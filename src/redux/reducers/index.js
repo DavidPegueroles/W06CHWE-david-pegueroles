0dfb5390821f52aca53590fc2111b0115db45b6e
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-20 14:14:49 +0100

    Added new reducer to index.js & fixed type errors in test

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import aRobotReducers from "./aRobotReducers";
 import robotsReducers from "./robotsReducers";
 
 const rootReducer = combineReducers({
   robots: robotsReducers,
+  robot: aRobotReducers,
 });
 
 export default rootReducer;
