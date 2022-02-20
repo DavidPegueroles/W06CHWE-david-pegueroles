e7dc840fe6ea42846e6bbebe327418fe4c8e5115
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-20 19:52:37 +0100

    Added getARobotThunk function

--- src/redux/thunks/robotsThunks.js
+++ src/redux/thunks/robotsThunks.js
@@ -1,8 +1,20 @@
-import { getRobotsAction } from "../actions/actionsCreators";
+import {
+  getARobotAction,
+  getRobotsAction,
+  robotCharacteristicsAction,
+} from "../actions/actionsCreators";
 
 export const getRobotsThunk = async (dispatch) => {
   const response = await fetch(process.env.REACT_APP_API_ROBOTS);
   const robots = await response.json();
 
   dispatch(getRobotsAction(robots.robots));
+};
+
+export const getARobotThunk = (id) => async (dispatch) => {
+  const response = await fetch(`${process.env.REACT_APP_API_ROBOTS}${id}`);
+  const robot = await response.json();
+
+  dispatch(robotCharacteristicsAction(robot.robot.characteristics));
+  dispatch(getARobotAction(robot.robot));
 };
