bc78216ce180baa950efb7928667665062fcfbb2
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-20 19:59:21 +0100

    Added another test of HomePage component

--- src/pages/HomePage/HomePage.test.js
+++ src/pages/HomePage/HomePage.test.js
@@ -3,9 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import renderWithProviders from "../../setupTests";
 import HomePage from "./HomePage";
 
-describe("Given a RobotDisplay component", () => {
-  describe("When it receives a robot", () => {
-    test("Then it should display a heading with the text robot", () => {
+describe("Given a HomePage component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should display a heading with the text robots list", () => {
       renderWithProviders(
         <BrowserRouter>
           <HomePage />
@@ -13,6 +13,18 @@ describe("Given a RobotDisplay component", () => {
       );
 
       const title = screen.getByRole("heading", { name: /robots list/i });
+
+      expect(title).toBeInTheDocument();
+    });
+
+    test("Then it should display a heading with the text robot", () => {
+      renderWithProviders(
+        <BrowserRouter>
+          <HomePage />
+        </BrowserRouter>
+      );
+
+      const title = screen.getByRole("heading", { name: /robot/i });
 
       expect(title).toBeInTheDocument();
     });
