6a750883c37dde1732500beaa47bae6e0e4d8006
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-20 19:33:08 +0100

    Updated states to new reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import aRobotReducers from "./aRobotReducers";
+import robotCharacteristicsReducers from "./robotCharacteristicsReducers";
 import robotsReducers from "./robotsReducers";
 
 const rootReducer = combineReducers({
   robots: robotsReducers,
   robot: aRobotReducers,
+  characteristics: robotCharacteristicsReducers,
 });
 
 export default rootReducer;
